8e767628cb588e69e0ad158ab40fb911f81fac8b
Moritz Mähr <moritz.maehr@gmail.com>
2020-04-09 18:01:58 +0200

    switch to webpack janus working

--- src/client.js
+++ src/client.js
@@ -1,5 +1,15 @@
 import * as sapper from '@sapper/app';
 
+import { Janus } from 'janus-gateway';
+
+Janus.init({
+   debug: true,
+   dependencies: Janus.useDefaultDependencies(), // or: Janus.useOldDependencies() to get the behaviour of previous Janus versions
+   callback: function() {
+           console.log("DONE")
+           }
+   });
+
 sapper.start({
 	target: document.querySelector('#sapper')
 });
