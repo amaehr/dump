a7b808cffbec2fb498a473665fe33e4868275e79
Moritz Mähr <moritz.maehr@gmail.com>
2020-04-10 15:09:20 +0200

    wip

--- src/routes/connect.svelte
+++ src/routes/connect.svelte
@@ -2,80 +2,105 @@
   import { onMount } from "svelte";
   import { Janus } from "janus-gateway";
 
+  let mixer = null;
+  let janus = null;
+
   onMount(() => {
     Janus.init({
       debug: true,
       dependencies: Janus.useDefaultDependencies(),
-      callback: function() {}
+      callback: function() {
+        console.log("init success");
+      }
     });
-
     let janus = new Janus({
       server: [
-        "ws://localhost:2095",
         "wss://localhost:2096",
-        "http://localhost:8080/janus",
-        "https://localhost:8443/janus"
+        "ws://localhost:2095",
+        "https://localhost:8443/janus",
+        "http://localhost:8080/janus"
       ],
       iceServers: [
+        "stun.solnet.ch:3478",
         "stun.l.google.com:19302",
         "stun1.l.google.com:19302",
         "stun2.l.google.com:19302",
         "stun3.l.google.com:19302",
-        "stun4.l.google.com:19302",
-        "stun.ekiga.net",
-        "stun.ideasip.com",
-        "stun.rixtelecom.se",
-        "stun.schlund.de",
-        "stun.stunprotocol.org:3478",
-        "stun.voiparound.com",
-        "stun.voipbuster.com",
-        "stun.voipstunt.com",
-        "stun.voxgratia.org"
+        "stun4.l.google.com:19302"
       ],
       success: function() {
         // Done! attach to plugin XYZ
-        console.log("success");
+        janus.attach({
+          plugin: "janus.plugin.audiobridge",
+          success: function(pluginHandle) {
+            // Plugin attached! 'pluginHandle' is our handle
+            mixer = pluginHandle;
+            Janus.log("Plugin attached!", pluginHandle);
+          },
+          error: function(cause) {
+            // Couldn't attach to the plugin
+            Janus.error("  -- Error attaching plugin...", cause);
+          },
+          consentDialog: function(on) {
+            // e.g., Darken the screen if on=true (getUserMedia incoming), restore it otherwise
+            Janus.debug(
+              "Consent dialog should be " + (on ? "on" : "off") + " now"
+            );
+            if (on) {
+              alert("consent missing");
+            }
+          },
+          onmessage: function(msg, jsep) {
+            // We got a message/event (msg) from the plugin
+            // If jsep is not null, this involves a WebRTC negotiation
+            Janus.debug(" ::: Got a message :::");
+            Janus.debug(msg);
+            var event = msg["audiobridge"];
+            Janus.debug("Event: " + event);
+          },
+          onlocalstream: function(stream) {
+            // We have a local stream (getUserMedia worked!) to display
+          },
+          onremotestream: function(stream) {
+            // We have a remote stream (working PeerConnection!) to display
+          },
+          oncleanup: function() {
+            // PeerConnection with the plugin closed, clean the UI
+            // The plugin handle is still valid so we can create a new one
+          },
+          detached: function() {
+            // Connection with the plugin closed, get rid of its features
+            // The plugin handle is not valid anymore
+          }
+        });
       },
       error: function(cause) {
         // Error, can't go on...
-        console.log("error");
+        alert("new janus error");
       },
       destroyed: function() {
         // I should get rid of this
       }
     });
-    // janus.attach({
-    //         plugin: "janus.plugin.echotest",
-    //         success: function (pluginHandle) {
-    //                 // Plugin attached! 'pluginHandle' is our handle
-    //                 console.log("echo läuft")
-    //         },
-    //         error: function (cause) {
-    //                 // Couldn't attach to the plugin
-    //         },
-    //         consentDialog: function (on) {
-    //                 // e.g., Darken the screen if on=true (getUserMedia incoming), restore it otherwise
-    //         },
-    //         onmessage: function (msg, jsep) {
-    //                 // We got a message/event (msg) from the plugin
-    //                 // If jsep is not null, this involves a WebRTC negotiation
-    //         },
-    //         onlocalstream: function (stream) {
-    //                 // We have a local stream (getUserMedia worked!) to display
-    //         },
-    //         onremotestream: function (stream) {
-    //                 // We have a remote stream (working PeerConnection!) to display
-    //         },
-    //         oncleanup: function () {
-    //                 // PeerConnection with the plugin closed, clean the UI
-    //                 // The plugin handle is still valid so we can create a new one
-    //         },
-    //         detached: function () {
-    //                 // Connection with the plugin closed, get rid of its features
-    //                 // The plugin handle is not valid anymore
-    //         }
-    // });
   });
+
+  let room = null
+
+  function register() {
+    // let register = { request: "join", room: 1234};
+    let register = { request: "join"};
+    mixer.send({ message: register });
+
+  }
+
+  let muted = false
+  function mute() {
+    // let register = { request: "join", room: 1234};
+    // mixer.send({ message: register });
+    muted = !muted
+    let mute = { request: "configure", muted: muted};
+    mixer.send({ message: mute});
+  }
 </script>
 
 <svelte:head>
@@ -84,3 +109,12 @@
 <h1>Audiochat</h1>
 
 <p>Hopefully soon</p>
+
+
+<button on:click={register}>
+join room
+</button>
+
+<button on:click={mute}>
+mute/unmute
+</button>
