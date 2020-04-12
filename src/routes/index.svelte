89444adf79f5e1c2a02c14f572c7efdb837808d3
Moritz Mähr <moritz.maehr@gmail.com>
2020-04-12 14:30:07 +0200

    stuff

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,17 @@
 <svelte:head>
-	<title>open-museum</title>
+  <title>open-museum</title>
 </svelte:head>
 
 <p>soon to be Startseite</p>
 
-<p><a rel=prefetch href="spanish-flu/7">Raum 7: Spanische Grippe</a></p>
-<p><a rel=prefetch href='about'>Raum XY: About (lang)</a></p>
+<p>
+  <a rel="prefetch" href="spanish-flu/7">Raum 7: Spanische Grippe</a>
+</p>
 
-<p><a rel=prefetch href='spanish-flu'>(raumliste)</a></p>
+<p>
+  <a rel="prefetch" href="spanish-flu">(raumliste)</a>
+</p>
 
-
-<p><a rel=prefetch href='connect'>(connect)</a></p>
+<p>
+  <a rel="prefetch" href="connect">(audio connect test)</a>
+</p>
